60831458d736cd4723feea0f519bcaacc1e9edc8
ufdilla <learnmore.cumi@gmail.com>
2017-12-14 13:25:35 +0700

    input file txt

--- inputFile.java
+++ inputFile.java
@@ -4,7 +4,18 @@ import java.io.*;
  */
 public class inputFile {
 
-    public static void main(String[] args) throws ioException{
-        
+    public static void main(String[] args) throws IOException {
+        FileInputStream Input1 = null;
+        FileOutputStream Ouput1 = null;
+
+        try 
+        {
+            Input1 = new FileInputStream("daftar_pustaka.txt");
+            Ouput1 = new FileOutputStream("daftar_Pustaka_Output.txt");
+        } 
+        catch (Exception e) 
+        {
+            e.getStackTrace();
+        }
     }
 }
