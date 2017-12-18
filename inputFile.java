d4abdcab427f3c9f13b5b5209f49dd4c93cd1e88
fadilla <learnmore.cumi@gmail.com>
2017-12-18 11:18:22 +0700

    memecah kalimat per spasi
    menghilangkan semua tanda titik, meletakkan titik setelah tanda tanya, memecah kalimat per spasi

--- inputFile.java
+++ inputFile.java
@@ -1,10 +1,34 @@
 import java.io.*;
+import java.util.*;
 /**
  * inputFile
  */
-public class inputFile {
+public class inputFile 
+{
 
-    public static void main(String[] args) throws ioException{
-        
+    public static void main (String[] args) throws IOException 
+    {
+        // membaca isi pada file  input.txt
+        BufferedReader in = new BufferedReader (new FileReader ("input.txt"));
+        String str, str1, str2, str3;
+        StringBuffer stringBuffer;
+        while ((str = in.readLine ()) != null) 
+            {
+            //menghilangkan semua dot
+            str1 = str.replaceAll ("[.]", ""); 
+            // meletkkan titik setelah quest mark
+            str2 = str.replaceAll ("[?]", "?.");
+
+            // memecah kalimat per spasi
+            for (String retval: str.split ("\\s"))
+                {
+                System.out.println (retval);
+                }
+
+            //menyisipkan karakter didalam string
+            str2 = new StringBuffer(str).insert(1, "zzzzz").toString();
+
+            System.out.println(str1);
+        }
     }
 }
