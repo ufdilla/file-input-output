d3e40ff3496c4e0c2722b9c2328682b1473e788f
ufdilla <learnmore.cumi@gmail.com>
2017-12-20 11:16:39 +0700

    pecah elemen nama, hilang titik, tambah titik after tanda tanya

--- inputFile.java
+++ inputFile.java
@@ -5,17 +5,19 @@ import java.util.regex.*;
 public class inputFile 
 {
 
-    public static void main(String args[]) 
+    public static void main(String args[]) throws IOException
     {
-         // String to be scanned to find the pattern. 
-        String line = "Abunadi, I. (2013). Influence of Culture on e-Government Acceptance in Saudi Arabia. arXiv preprint arXiv:1307.7141."; 
-        String pattern = "(([A-Za-z]+),(( [A-Z].)+))"; 
-         
+        // String to be scanned to find the pattern. 
+        BufferedReader in = new BufferedReader( new FileReader("input.txt"));
+        String data = in.readLine();
+        String dataString = data.replaceAll("[.]", "").replaceAll("[?]","?.");
+        
         // Create a Pattern object 
+        String pattern = "(([A-Za-z]+),(( [A-Z].)+))"; 
         Pattern r = Pattern.compile(pattern); 
 
           // Now create matcher object. 
-        Matcher m = r.matcher(line); 
+        Matcher m = r.matcher(dataString); 
         if (m.find())  
         { 
             System.out.println("Found value: " + m.group(1)); 
