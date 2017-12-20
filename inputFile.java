c1732abcc2bddc362368986fe3b50e108880b2a7
ufdilla <learnmore.cumi@gmail.com>
2017-12-20 12:04:29 +0700

    merubah nama belakang <-> nama depan dengan regex

--- inputFile.java
+++ inputFile.java
@@ -5,28 +5,34 @@ import java.util.regex.*;
 public class inputFile 
 {
 
-    public static void main(String args[]) throws IOException
+    public static void main (String args[]) throws IOException
     {
         // String to be scanned to find the pattern. 
-        BufferedReader in = new BufferedReader( new FileReader("input.txt"));
-        String data = in.readLine();
-        String dataString = data.replaceAll("[.]", "").replaceAll("[?]","?.");
+        BufferedReader in = new BufferedReader ( new FileReader ("input.txt"));
+        String data = in.readLine ();
+        String dataString = data.replaceAll ("[.]", "").replaceAll ("[?]","?.");
         
         // Create a Pattern object 
         String pattern = "(([A-Za-z]+),(( [A-Z].)+))"; 
-        Pattern r = Pattern.compile(pattern); 
+        Pattern rowPattern = Pattern.compile (pattern); 
 
           // Now create matcher object. 
-        Matcher m = r.matcher(dataString); 
-        if (m.find())  
+        Matcher matcher1 = rowPattern.matcher (dataString); 
+        if (matcher1.find ())  
         { 
-            System.out.println("Found value: " + m.group(1)); 
-            System.out.println("Found value: " + m.group(2)); 
-            System.out.println("Found value: " + m.group(3)); 
+            String fullName = matcher1.group (1);
+            String firstName = matcher1.group (2);
+            String lastName = matcher1.group (3);
+
+            Pattern rowPattern2 = Pattern.compile(fullName);
+            Matcher matcher2 = rowPattern2.matcher(dataString);
+            dataString = matcher2.replaceAll(lastName+firstName+"; ");
+
+            System.out.println ("Nama Pengarang: " + dataString);
         }  
         else  
         { 
-            System.out.println("NO MATCH"); 
+            System.out.println ("NO MATCH"); 
         } 
     }
 }
