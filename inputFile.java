964dbae5b90f3f9b59a73709053de04fd82c539a
ufdilla <learnmore.cumi@gmail.com>
2017-12-20 14:05:19 +0700

    menghilangkan tanda kurung pada tahun buku

--- inputFile.java
+++ inputFile.java
@@ -4,35 +4,44 @@ import java.util.regex.*;
 
 public class inputFile 
 {
-
     public static void main (String args[]) throws IOException
     {
         // String to be scanned to find the pattern. 
         BufferedReader in = new BufferedReader ( new FileReader ("input.txt"));
-        String data = in.readLine ();
-        String dataString = data.replaceAll ("[.]", "").replaceAll ("[?]","?.");
-        
-        // Create a Pattern object 
-        String pattern = "(([A-Za-z]+),(( [A-Z].)+))"; 
-        Pattern rowPattern = Pattern.compile (pattern); 
+        FileOutputStream out = new FileOutputStream ("output.txt");
+        String data;
 
-          // Now create matcher object. 
-        Matcher matcher1 = rowPattern.matcher (dataString); 
-        if (matcher1.find ())  
-        { 
-            String fullName = matcher1.group (1);
-            String firstName = matcher1.group (2);
-            String lastName = matcher1.group (3);
+        while ((data  = in.readLine ()) != null) 
+        {
+            String dataString = data.replaceAll ("[.]", "").replaceAll ("[?]","?.").replaceAll("[(]","").replaceAll("[)]", ".");
+            
+            // Create a Pattern object 
+            String pattern = "(([A-Za-z]+),(( [A-Z].)+))"; 
+            Pattern rowPattern = Pattern.compile (pattern); 
 
-            Pattern rowPattern2 = Pattern.compile(fullName);
-            Matcher matcher2 = rowPattern2.matcher(dataString);
-            dataString = matcher2.replaceAll(lastName+firstName+"; ");
+            //Create matcher object. 
+            Matcher matcher1 = rowPattern.matcher (dataString); 
+            
+            if (matcher1.find ())  
+            { 
+                String fullName = matcher1.group (1);
+                String firstName = matcher1.group (2);
+                String lastName = matcher1.group (3);
 
-            System.out.println ("Nama Pengarang: " + dataString);
-        }  
-        else  
-        { 
-            System.out.println ("NO MATCH"); 
-        } 
+                Pattern rowPattern2 = Pattern.compile(fullName);
+                Matcher matcher2 = rowPattern2.matcher(dataString);
+                dataString = matcher2.replaceAll(lastName + firstName + "; ");
+
+                System.out.println (dataString);
+                // out.write(dataString);
+            }  
+            
+            else  
+            { 
+                System.out.println ("NO MATCH"); 
+            } 
+        }
+
+        in.close();
     }
 }
