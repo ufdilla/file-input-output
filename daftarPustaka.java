32e1be5a044c3ddc992f4d46cbb31e7053d64fe3
ufdilla <learnmore.cumi@gmail.com>
2017-12-28 10:35:38 +0700

    trial 02

--- daftarPustaka.java
+++ daftarPustaka.java
@@ -8,37 +8,51 @@ public class daftarPustaka
     {
         // String to be scanned to find the pattern. 
         BufferedReader in = new BufferedReader (new FileReader ("input.txt"));
-        BufferedWriter out = new BufferedWriter (new FileWriter("output.txt"));
+        BufferedWriter out = new BufferedWriter (new FileWriter ("output.txt"));
         String data;
+        StringBuffer stringBuffer1;
+        stringBuffer1 = new StringBuffer();
  
         while ((data  = in.readLine ()) != null) 
         {
-            String dataString = data.replaceAll ("[.]", "").replaceAll ("[?]","?.").replaceAll("[(]","").replaceAll("[)]", ".");
+            stringBuffer1.append(data);
+            stringBuffer1.append("\n");
+            String[] a = stringBuffer1.toString().split("\n");
             
-            // Create a Pattern object 
-            String pattern = "(([A-Za-z]+),(( [A-Z].)+))"; 
-            Pattern rowPattern = Pattern.compile (pattern); 
- 
-            //Create matcher object. 
-            Matcher matcher1 = rowPattern.matcher (dataString); 
-            
-            if (matcher1.find ())  
-            { 
-                String fullName = matcher1.group (1);
-                String firstName = matcher1.group (2);
-                String lastName = matcher1.group (3);
-                Pattern rowPattern2 = Pattern.compile(fullName);
-                Matcher matcher2 = rowPattern2.matcher(dataString);
-                dataString = matcher2.replaceAll(lastName + firstName + "; ");
+            for (int i = 0; i < 2; i++) {
+                String kata = a[i].replaceAll("\\.", "").replaceAll("\\?", "\\?.");
+                String[] b = kata.split(" ");
+                System.out.print(b[1] + ". " + b[0] + " ");
 
-                System.out.println(dataString);
-                out.newLine();
-                ;
-                out.write(dataString);
+                for (int j = 2; j < b.length; j++) {
+                    System.out.print(b[j] + " ");
+                }
+                System.out.println(".");
+                // out.write("");
             }
 
-            else {
-                System.out.println("NO MATCH");
+            String kata2 = a[2].replaceAll("\\.", "").replaceAll("\\?", "\\?.");
+            String[] c = kata2.split(" ");
+            for (int k = 0; k < 1; k++) {
+                System.out.print(
+                        c[1] + " " + c[0] + " " + c[3] + " " + c[2] + " " + c[4] + " " + c[6] + " " + c[5] + "; ");
+                for (int l = 6; l < c.length; l++) {
+                    System.out.print(c[l] + " ");
+                }
+
+                System.out.println(".");
+            }
+
+            String kata3 = a[3].replaceAll("\\.", "").replaceAll("\\?", "\\?.");
+            String[] d = kata3.split(" ");
+
+            for (int m = 0; m < 1; m++) {
+                System.out.print(d[1] + ". " + d[0] + " ");
+                for (int n = 2; n < d.length; n++) {
+                    System.out.print(d[n] + " ");
+                }
+
+                System.out.println(".");
             }
         }
 
--- inputFile.java
+++ inputFile.java
@@ -4,58 +4,74 @@ import java.util.regex.*;
 
 public class inputFile {
 
-    public static void main(String[] args) {
-        try {
-            File file = new File("input.txt");
-            StringBuffer stringBuffer;
+    public static void main(String[] args) 
+    {
+        try 
+        {
+            File file1 = new File ("input.txt");
+            StringBuffer stringBuffer1;
  
-            try (FileReader fileReader = new FileReader(file)) {
-                BufferedReader bufferedReader = new BufferedReader(fileReader);
-                stringBuffer = new StringBuffer();
+            try (FileReader fileReader1 = new FileReader (file1 )) 
+            {
+                BufferedReader bufferedReader = new BufferedReader (fileReader1);
+                BufferedWriter out = new BufferedWriter(new FileWriter("output.txt"));
+
+                stringBuffer1 = new StringBuffer ();
                 String name;
-                while ((name = bufferedReader.readLine()) != null) {
-                    stringBuffer.append(name);
-                    stringBuffer.append("\n");
+                while ((name = bufferedReader.readLine ()) != null) 
+                {
+                    stringBuffer1.append (name);
+                    stringBuffer1.append ("\n");
                 }
                 
-                String[] a = stringBuffer.toString().split("\n");
+                String[] a = stringBuffer1.toString ().split ("\n");
                 // List<String> list = new ArrayList<>();
  
-                for (int i = 0; i < 2; i++) {
-                    String kata = a[i].replaceAll("\\.", "").replaceAll("\\?", "\\?.");
-                    String[] b = kata.split(" ");
-                    System.out.print(b[1] + ". " + b[0] + " ");
+                for (int i = 0; i < 2; i++) 
+                {
+                    String kata = a[i].replaceAll ("\\.", "").replaceAll ("\\?", "\\?.");
+                    String[] b = kata.split (" ");
+                    System.out.print (b[1] + ". " + b[0] + " ");
  
-                    for (int j = 2; j < b.length; j++) {
-                        System.out.print(b[j] + " ");
-                    }
-    System.out.println(".");
-                }
- 
-                String kata2 = a[2].replaceAll("\\.", "").replaceAll("\\?", "\\?.");
-                String[] c = kata2.split(" ");
-                for (int k = 0; k < 1; k++) {
-                    System.out.print(
-                            c[1] + " " + c[0] + " " + c[3] + " " + c[2] + " " + c[4] + " " + c[6] + " " + c[5] + "; ");
-                    for (int l = 6; l < c.length; l++) {
-                        System.out.print(c[l] + " ");
+                    for (int j = 2; j < b.length; j++) 
+                    {
+                        System.out.print (b[j] + " ");
+                        // System.out.println("b[j] : " + b[j]);
                     }
                     System.out.println(".");
                 }
  
-                String kata3 = a[3].replaceAll("\\.", "").replaceAll("\\?", "\\?.");
-                String[] d = kata3.split(" ");
- 
-                for (int m = 0; m < 1; m++) {
-                    System.out.print(d[1] + ". " + d[0] + " ");
-                    for (int n = 2; n < d.length; n++) {
-                        System.out.print(d[n] + " ");
+                String kata2 = a[2].replaceAll ("\\.", "").replaceAll ("\\?", "\\?.");
+                String[] c = kata2.split (" ");
+                for (int k = 0; k < 1; k++) 
+                {
+                    System.out.print (c[1] + " " + c[0] + " " + c[3] + " " + c[2] + " " + c[4] + " " + c[6] + " " + c[5] + "; ");
+                    for (int l = 6; l < c.length; l++) 
+                    {
+                        System.out.print (c[l] + " ");
                     }
-                    System.out.println(".");
+
+                    System.out.println (".");
+                }
+ 
+                String kata3 = a[3].replaceAll ("\\.", "").replaceAll ("\\?", "\\?.");
+                String[] d = kata3.split (" ");
+ 
+                for (int m = 0; m < 1; m++) 
+                {
+                    System.out.print (d[1] + ". " + d[0] + " ");
+                    for (int n = 2; n < d.length; n++) 
+                    {
+                        System.out.print (d[n] + " ");
+                    }
+
+                    System.out.println (".");
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace ();
         }
     }
 }
