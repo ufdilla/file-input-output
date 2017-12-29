80832e936af10db09d282a38a20f704e527f1245
ufdilla <learnmore.cumi@gmail.com>
2017-12-29 09:37:25 +0700

    merapikan code (standart pak bernaridho)

--- inputFile.java
+++ inputFile.java
@@ -14,11 +14,11 @@ public class inputFile
             try (FileReader fileReader1 = new FileReader (file1 )) 
             {
                 BufferedReader bufferedReader = new BufferedReader (fileReader1);
-                BufferedWriter out = new BufferedWriter(new FileWriter ("output.txt", true));
-
+                BufferedWriter output1 = new BufferedWriter(new FileWriter ("output.txt", true));
+ 
                 stringBuffer1 = new StringBuffer ();
                 String name;
-
+ 
                 while ((name = bufferedReader.readLine ()) != null) 
                 {
                     stringBuffer1.append (name);
@@ -35,11 +35,11 @@ public class inputFile
                     for (int j = 2; j < b.length; j++) 
                     {
                         System.out.print (b[j] + " ");
-                        out.newLine();
-                        out.write(b[j]);
+                        output1.newLine ();
+                        output1.write (b[j]);
                     }
-
-                    System.out.println(".");
+ 
+                    System.out.println (".");
                 }
  
                 String kata2 = a[2].replaceAll ("\\.", "").replaceAll ("\\?", "\\?.");
@@ -50,10 +50,10 @@ public class inputFile
                     for (int l = 6; l < c.length; l++) 
                     {
                         System.out.print (c[l] + " ");
-                        out.newLine();
-                        out.write(c[l]);
+                        output1.newLine ();
+                        output1.write (c[l]);
                     }
-
+ 
                     System.out.println (".");
                 }
  
@@ -64,17 +64,17 @@ public class inputFile
                     System.out.print (d[1] + ". " + d[0] + " ");
                     for (int n = 2; n < d.length; n++) 
                     {
-                        System.out.print (d[n] + " ");
-                    out.newLine ();
-                    out.write (d[n]);
-                }
-
+                    System.out.print (d[n] + " ");
+                    output1.newLine ();
+                    output1.write (d[n]);
+                    }
+ 
                     System.out.println (".");
                 }
                
-                out.close ();
+                output1.close ();
             }
-        } 
+        }
 
         catch (IOException e) 
         {
